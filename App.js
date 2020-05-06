e86700e740a23c92e99592ce9647532150a4961d
vishaljkk <vishal.kk@somaiaya.edu>
2020-05-06 12:15:14 +0530

    with image loader

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View,Text,Image,StyleSheet} from 'react-native';
+
+
+const App = () => {
+  return (
+      <View style={styles.container}>
+        <Text style={styles.text} Hello World>
+          Hello World
+        </Text>
+        <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img}/>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  text:{
+    color:'darkslateblue', 
+    fontSize: 30 
+  },
+  img:{
+    width:100,
+    height:100
+  }
+
+})
+
+
+export default App;
