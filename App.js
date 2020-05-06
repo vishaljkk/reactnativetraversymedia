38102921e1a009d447239ff45cd80c664f167243
vishaljkk <vishal.kk@somaiaya.edu>
2020-05-06 19:21:00 +0530

    finally completed this course

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View,Text,StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+
+import AddItem from './components/AddItem'; 
 
 const App = () => {
+  const [items,setItems] = useState([
+    {id:Math.floor(Math.random() * 1000) ,text:'Milk'},
+    {id:Math.floor(Math.random() * 1000) ,text:'Eggs'},
+    {id:Math.floor(Math.random() * 1000) ,text:'Bread'},
+    {id:Math.floor(Math.random() * 1000) ,text:'Juice'}
+  ]);
+
+
+  const deleteItem = (id) =>{
+    setItems(prevItems=>{
+      return prevItems.filter(item=>item.id!=id);
+    });
+  };
+  const addItem = (text) =>{
+    setItems(prevItems=>{
+      console.log("adding item")
+      return [{id:Math.floor(Math.random() * 1000),text},...prevItems];
+    });
+  };
   return (
       <View style={styles.container}>
         <Header title='Shpooing List'/>
+        <AddItem addItem={addItem}/>
+        <FlatList 
+          data={items} renderItem={({item}) => (
+            <ListItem item={item} deleteItem={deleteItem}/>
+          )} />
       </View>
   );
 };
